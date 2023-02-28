c906785defb9c19f0107652e88d36f8b67d75797
Kyle Scott <kyscott@umich.edu>
2023-02-28 08:45:16 -0500

    use create3 to deploy

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -4,28 +4,35 @@ pragma solidity ^0.8.13;
 import { Script } from "forge-std/Script.sol";
 import { console2 } from "forge-std/console2.sol";
 
+import { CREATE3Factory } from "create3-factory/CREATE3Factory.sol";
+
 import { Factory } from "src/core/Factory.sol";
 import { LiquidityManager } from "src/periphery/LiquidityManager.sol";
 import { LendgineRouter } from "src/periphery/LendgineRouter.sol";
-import { Lendgine } from "src/core/Lendgine.sol";
 
 contract Deploy is Script {
+  address constant create3Factory = 0x93FEC2C00BfE902F733B57c5a6CeeD7CD1384AE1;
+
   address constant uniV2Factory = 0xc35DADB65012eC5796536bD9864eD8773aBc74C4;
   address constant uniV3Factory = 0x1F98431c8aD98523631AE4a59f267346ea31F984;
 
-  address constant weth = 0x82aF49447D8a07e3bd95BD0d56f35241523fBab1;
+  address constant weth = 0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270;
 
-  function run() external returns (Factory, LiquidityManager, LendgineRouter) {
+  function run() external returns (address factory, address liquidityManager, address lendgineRouter) {
+    CREATE3Factory create3 = CREATE3Factory(create3Factory);
+
     uint256 pk = vm.envUint("PRIVATE_KEY");
     vm.startBroadcast(pk);
 
-    Factory factory = new Factory{salt: keccak256("NumoenFactory1")}();
-    LiquidityManager liquidityManager =
-      new LiquidityManager{salt: keccak256("NumoenLiquidityManager1")}(address(factory), weth);
-    LendgineRouter lendgineRouter =
-    new LendgineRouter{salt: keccak256("NumoenLendgineRouter1")}(address(factory), uniV2Factory, uniV3Factory,
-    weth);
+    factory = create3.deploy(keccak256("NumoenFactory"), type(Factory).creationCode);
 
-    return (factory, liquidityManager, lendgineRouter);
+    liquidityManager = create3.deploy(
+      keccak256("NumoenLiquidityManager"), bytes.concat(type(LiquidityManager).creationCode, abi.encode(factory, weth))
+    );
+
+    lendgineRouter = create3.deploy(
+      keccak256("NumoenLendgineRouter"),
+      bytes.concat(type(LendgineRouter).creationCode, abi.encode(factory, uniV2Factory, uniV3Factory, weth))
+    );
   }
 }
